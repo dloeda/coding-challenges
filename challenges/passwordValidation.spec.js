03e25507ae224026026c74cdd0b55a41003eba4f
dloeda <diego.loeda@gmail.com>
2017-04-04 16:29:14 +0200

    ✨  feat: Add validate password

---
+++ challenges/passwordValidation.spec.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+const sinonChai = require("sinon-chai")
+
+chai.use(sinonChai)
+
+/** DESCRIPTION
+ * You need to write regex that will validate a password to make sure it meets the following criteria:
+ *
+ *    At least six characters long
+ *    contains a lowercase letter
+ *    contains an uppercase letter
+ *    contains a number
+ *
+ * Valid passwords will only be alphanumeric characters.
+ */
+
+/** SOLUTION */
+function validate(password) {
+  return /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password)
+}
+
+/** TESTS */
+
+describe('validatePassword', () => {
+  it('input  => output fasle', () => {
+    expect(validate('djI38D55')).to.be.true
+  })
+  it('input  => output fasle', () => {
+    expect(validate('a2.d412')).to.be.false
+  })
+  it('input  => output fasle', () => {
+    expect(validate('JHD5FJ53')).to.be.false
+  })
+  it('input  => output fasle', () => {
+    expect(validate('!fdjn345')).to.be.false
+  })
+  it('input  => output fasle', () => {
+    expect(validate('123')).to.be.false
+  })
+  it('input  => output fasle', () => {
+    expect(validate('abc')).to.be.false
+  })
+  it('input  => output fasle', () => {
+    expect(validate('Password123')).to.be.true
+  })
+})
